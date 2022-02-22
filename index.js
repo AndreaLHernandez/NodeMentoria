694768efa5031bfe2b10fdb720586afb0c1a8b1d
Andy Hernandez <ada_hack244@hackademy.mx>
2022-02-21 23:35:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 5000;//puede ser cualquier puerto
+
+app.get('/get', (req, res) => {
+    return res.status(200).send('Hola desde express');
+    // res.send('Hello World!')
+
+});
+
+app.listen(port, () =>{
+     console.log(`Listening on port ${port}!`)
+});
+
 console.log('Hello World xd');
